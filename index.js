ee809a90d150082367c65913ae3aa2c91e2a0cae
imranmustafa030 <imran94mustafa@gmail.com>
2023-03-06 00:17:56 +0500

    Todo List with Email sending Form

---
+++ index.js
@@ -0,0 +1,32 @@
+const todoList = document.querySelector(".todo-list");
+const todoForm = document.querySelector(".form-todo");
+const todoInput = document.querySelector(".form-todo input[type = 'text']");
+const newtodoList = document.querySelector(".todo-list");
+
+todoForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const newTodoText = todoInput.value;
+    const newLi = document.createElement("li");
+    const newLiInnerHtml = `
+        <span class="text">${newTodoText}</span>
+        <div class="todo-buttons">
+            <button class="todo-btn done">Done</button>
+            <button class="todo-btn remove">Remove</button>
+        </div>`;
+    newLi.innerHTML = newLiInnerHtml;
+    newtodoList.append(newLi);
+    todoInput.value = "";
+
+});
+
+todoList.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("remove")){
+        const targetedLi = e.target.parentNode.parentNode;
+        targetedLi.remove();
+    }
+    if(e.target.classList.contains("done")){
+        const liSpan = e.target.parentNode.previousElementSibling;
+        liSpan.style.textDecoration = "line-through";
+    }
+    
+})
